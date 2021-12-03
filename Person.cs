9654f14e1caa1cdaa2cb89a8db32d0d0135c6de1
Saewran <Gadzillos@gmail.com>
2021-12-03 05:13:18 +0300

    initial commit

---
+++ Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Homework_2
+{
+    public class Person
+    {
+        private string name;
+        private string email;
+        private TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        private const int EmailLengthLimit = 254;// According to spec with clarification from IETF RFC Errata in 2010
+        private const int NameLengthLimit = 50;
+
+        public Person(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null) { throw new ArgumentNullException("Karma police\nArrest this null"); }
+                value = value.Trim();
+                if (value == "" ) { throw new ArgumentException("Empty name"); }
+                if (value.Length > NameLengthLimit) { throw new ArgumentOutOfRangeException(); }
+                value = value.ToLower();
+                value = textInfo.ToTitleCase(value);
+                value = value.Replace("<", "&lt;");
+                value = value.Replace(">", "&gt;");
+                name = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value == null) { throw new ArgumentNullException("Karma police\nArrest this null"); } //System.Net.Mail.MailAddress throws same exception
+                value = value.Trim();
+                if (value == "") { throw new ArgumentException("Empty string for email"); }
+                if (value.Length > EmailLengthLimit) { throw new ArgumentOutOfRangeException(); }
+                if (IsValidEmail(value)) { email = value; }
+                else { throw new ArgumentException("Email is invalid\n" + $"{value}\n"); }
+            }
+        }
+
+        protected bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Homework_2
+{
+    class Program
+    {
+        public static IList<Person> people = new List<Person>();
+
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\index.html";
+            people.Add(new Person("  X Æ A-12  ", "IsThereLifeOnMars@protonmail.com"));
+            people.Add(new Person("Maxim<script>alert('Name!')</script>", "Scripter1337@yahoo.com"));
+            people.Add(new Person("AlexanDRIA          \nTEnWhiteSpaces tripLeName   ", "validmail@gmail.com"));
+            people.Add(new Person("WHOOPSCAPSLOCK", "validmail@gmail.com"));
+            people.Add(new Person("Doge" + "\u003C" + "script" + "\u003E" + "alert('BONK!')" + "\u003C" + "/script" + "\u003E", "Doge@gmail.com"));
+            people.Add(new Person("null", "okNull@gmail.com"));
+            people.Add(new Person("кириллица", "cyrillic@mail.ru"));
+            people.Add(new Person("シャーマンキング", "シャーマンキング@gmail.com"));
+
+            string htmlString = BuildHtmlString(people);
+
+            try
+            {
+                File.WriteAllText(path, htmlString);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("I/O error");
+                throw e;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("*Bonk*");
+                throw e; 
+            }
+        }
+
+        public static string BuildHtmlString(IList<Person> list)
+        {
+            StringBuilder htmlBuilder = new StringBuilder("<!DOCTYPE html>\n" + "<html>\n" + "<body>");
+            foreach (Person person in people)
+            {
+                htmlBuilder.Append($"\n<a href=\"mailto: {person.Email}\">{person.Name}</a> |");
+            }
+            htmlBuilder.Remove(htmlBuilder.Length - 1, 1);
+            htmlBuilder.Append("\n</body>" + "\n</html>");
+            return htmlBuilder.ToString();
+        }
+    }
+}
